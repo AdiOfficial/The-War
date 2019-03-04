8dfe7d8782dcd27e08cb929ccb5d2ea8d284b2c2
kotvitz17@gmail.com <kotvitz17@gmail.com>
2019-03-05 00:21:38 +0100

    added the option to change a language in the menu

--- app/src/main/java/com/game/kotvitz/war/LanguageChoice.java
+++ app/src/main/java/com/game/kotvitz/war/LanguageChoice.java
@@ -1,15 +1,11 @@
 package com.game.kotvitz.war;
 
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.util.Locale;
 
 public class LanguageChoice extends AppCompatActivity {
 
@@ -23,7 +19,7 @@ public class LanguageChoice extends AppCompatActivity {
         langEn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setLocale("en-us", mainMenu, getResources());
+                LocaleManager.setLocale("en-us", mainMenu, getResources());
                 startActivity(mainMenu);
             }
         });
@@ -31,7 +27,7 @@ public class LanguageChoice extends AppCompatActivity {
         langPol.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setLocale("pl", mainMenu, getResources());
+                LocaleManager.setLocale("pl", mainMenu, getResources());
                 startActivity(mainMenu);
             }
         });
@@ -40,14 +36,5 @@ public class LanguageChoice extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
-    }
-
-    public void setLocale(String localeName, Intent intent, Resources res) {
-        Locale myLocale = new Locale(localeName);
-        DisplayMetrics dm = res.getDisplayMetrics();
-        Configuration conf = res.getConfiguration();
-        conf.locale = myLocale;
-        res.updateConfiguration(conf, dm);
-        intent.putExtra("lang", localeName);
     }
 }
--- app/src/main/java/com/game/kotvitz/war/OptionsAcitvity.java
+++ app/src/main/java/com/game/kotvitz/war/OptionsAcitvity.java
@@ -1,23 +1,65 @@
 package com.game.kotvitz.war;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+
+import java.util.Locale;
+import java.util.Objects;
 
 public class OptionsAcitvity extends AppCompatActivity {
+
+    private Button backButton;
+    private RadioButton radioPolish;
+    private RadioButton radioEnglish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
         ScreenDesigner.callFullScreenMode(getWindow());
-        Button backButton = findViewById(R.id.backButtonOptions);
+        checkLanguage();
+        backButton = findViewById(R.id.backButtonOptions);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.pl_lang:
+                if (checked) {
+                    Intent intent = new Intent(this, MainMenuActivity.class);
+                    LocaleManager.setLocale("pl", intent, getResources());
+                    startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                            Intent.FLAG_ACTIVITY_NEW_TASK));
+                }
+                break;
+            case R.id.eng_lang:
+                if (checked) {
+                    Intent intent = new Intent(this, MainMenuActivity.class);
+                    LocaleManager.setLocale("en-us", intent, getResources());
+                    startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                            Intent.FLAG_ACTIVITY_NEW_TASK));
+                }
+                break;
+        }
+    }
+
+    public void checkLanguage() {
+        Locale currentLocale = getResources().getConfiguration().locale;
+        radioEnglish = findViewById(R.id.eng_lang);
+        radioPolish = findViewById(R.id.pl_lang);
+        if (Objects.equals(currentLocale.getLanguage(), "en-us"))
+            radioEnglish.setChecked(true);
+        else if (Objects.equals(currentLocale.getLanguage(), "pl"))
+            radioPolish.setChecked(true);
     }
 }
