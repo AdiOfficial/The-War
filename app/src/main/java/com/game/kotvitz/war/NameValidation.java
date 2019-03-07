813e9fb6fd649b88b2e45e2eca72c9f46dc5dc12
kotvitz17@gmail.com <kotvitz17@gmail.com>
2019-03-07 13:20:51 +0100

    Aligned texts on game toolbar + reduced the number of characters allowed in the player's name

--- app/src/main/java/com/game/kotvitz/war/NameValidation.java
+++ app/src/main/java/com/game/kotvitz/war/NameValidation.java
@@ -2,7 +2,7 @@ package com.game.kotvitz.war;
 
 public class NameValidation {
 
-    private static final String nameRegex = "^(?=.*[A-Za-z]).{1,12}$";
+    private static final String nameRegex = "^(?=.*[A-Za-z]).{1,10}$";
 
     public static boolean validateName(String name) {
         return name.matches(nameRegex);
